be387718ae1d919b2a35e2b2558e56da6459b125
Taylor Riggan <triggan@gmail.com>
2019-03-14 18:56:52 -0400

    Updated to support .NET Core 3.0.0-preview

--- Program.cs
+++ Program.cs
@@ -140,10 +140,6 @@ namespace Aws4RequestSigner
 
             canonical_request.Append(signed_headers + "\n");
 
-            /*  var content = new byte[0];
-            if (request.Content != null) {
-                content = request.Content.ReadAsByteArrayAsync();
-            } */
             var content =  new byte[0];
             var payload_hash = Hash(content);
 
@@ -156,8 +152,7 @@ namespace Aws4RequestSigner
             var signature = ToHexString(HmacSHA256(signing_key, string_to_sign));
             
             request.Headers.TryAddWithoutValidation("Authorization", $"{algorithm} Credential={_access_key}/{credential_scope}, SignedHeaders={signed_headers}, Signature={signature}");
-            //request.Headers.TryAddWithoutValidation("Authorization", "Credential={_access_key}/{credential_scope}, SignedHeaders={signed_headers}, Signature={signature}");
-
+            
             return request;
         }
 
@@ -177,34 +172,52 @@ namespace Aws4RequestSigner
         static void Main(string[] args)
         {
             
-            var neptunesigner = new AWS4RequestSigner("","");
+            var access_key = "";
+            var secret_key = "";
+            var neptune_endpoint = ""; // ex: mycluster.cluster.us-east-1.neptune.amazonaws.com
+            var neptune_region = ""; //ex: us-east-1
+            var using_ELB = false; //Set to True if using and ELB and define ELB URL via ELB_endpoint variable
+            var ELB_endpoint = ""; //ex: myelb.elb.us-east-1.amazonaws.com
+
+            /* The AWS4RequestSigner library was intented to pass a signed request to an HTTP endpoint.
+             * Since we're using websockets, we will create the HTTP request and sign the request, however
+             * we will pull the headers from the signed request in order to create a webSocketConfiguration
+             * object with these same headers. 
+             */
+            var neptunesigner = new AWS4RequestSigner(access_key,secret_key);
             var request = new HttpRequestMessage {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri("http://neptunedbcluster-wrtac1r4zcpr.cluster-cbhb3echwkcs.us-east-2.neptune.amazonaws.com:8182/gremlin")
+                RequestUri = new Uri("http://" + neptune_endpoint + "/gremlin")
             };
-            var signedrequest = neptunesigner.Sign(request, "neptune-db", "us-east-2");
+            var signedrequest = neptunesigner.Sign(request, "neptune-db", neptune_region);
             var authText = signedrequest.Headers.GetValues("Authorization").FirstOrDefault();
-            Console.WriteLine(authText);
-
-            /*
-            var authPlainText = System.Text.Encoding.UTF8.GetBytes(authText);
-            var authBase64 = System.Convert.ToBase64String(authPlainText);
-            */
+            var authDate = signedrequest.Headers.GetValues("x-amz-date").FirstOrDefault();
 
             var webSocketConfiguration = new Action<ClientWebSocketOptions>(options => { 
-                    options.SetRequestHeader("host", "neptunedbcluster-wrtac1r4zcpr.cluster-cbhb3echwkcs.us-east-2.neptune.amazonaws.com:8182");
-                    options.SetRequestHeader("x-amz-date", signedrequest.Headers.GetValues("x-amz-date").FirstOrDefault());
+                    options.SetRequestHeader("host", neptune_endpoint);
+                    options.SetRequestHeader("x-amz-date", authDate);
                     options.SetRequestHeader("Authorization",authText);
                     }); 
 
-            Console.WriteLine("Postsigned request" + signedrequest.ToString());
-            var gremlinServer = new GremlinServer("neptune-f03bb540929fba64.elb.us-east-2.amazonaws.com", 8182);
+            /* GremlinServer() accepts the hostname and port as separate parameters.  
+             * Split the endpoint into both variables to pass to GremlinServer()
+             *
+             * Also - determine if an ELB is used.  If using an ELB, connect using the ELB hostname.
+             */
+            var neptune_host = ""; 
+            if(using_ELB) {
+                neptune_host = ELB_endpoint;
+            } else {
+                neptune_host = neptune_endpoint.Split(':')[0];
+            }
+            var neptune_port = int.Parse(neptune_endpoint.Split(':')[1]);
+
+            var gremlinServer = new GremlinServer(neptune_host, neptune_port);
             var gremlinClient = new GremlinClient(gremlinServer, webSocketConfiguration: webSocketConfiguration);
-
             var remoteConnection = new DriverRemoteConnection(gremlinClient);
-
             var g = Traversal().WithRemote(remoteConnection);
 
+            /* Example code to pull the first 5 vertices in a graph. */
             Console.WriteLine("Get List of Node Labels:");
             Int32 limitValue = 5;
             var output = g.V().Limit<Vertex>(limitValue).ToList();
